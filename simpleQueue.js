190a75bc30efaa8b442ee179a51e691f64c88706
ibnu <ibnu@SPL-Aurora.local>
2019-08-13 11:44:07 +0700

    feat: simple queue consumer

---
+++ simpleQueue.js
@@ -0,0 +1,54 @@
+const RedisSMQ = require("rsmq");
+const rsmq = new RedisSMQ( {host: "127.0.0.1", port: 6379, ns: "rsmq"} );
+
+function main() {
+	const queuename = "testqueue";
+
+	// create a queue
+	rsmq.createQueue({ qname: queuename }, (err) => {
+		if (err) {
+			// if the error is `queueExists` we can keep going as it tells us that the queue is already there
+			if (err.name !== "queueExists") {
+				console.error(err);
+				return;
+			} else {
+				console.log("queue exists.. resuming..");
+			}
+		}
+
+		// start checking for messages every 500ms
+		receiveMessageLoop(queuename);
+	});
+}
+
+main();
+
+function receiveMessageLoop(queuename) {
+	// check for new messages every second
+	setInterval(() => {
+		// alternative to receiveMessage would be popMessage => receives the next message from the queue and deletes it.
+		rsmq.receiveMessage({ qname: queuename }, (err, resp) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+
+			// checks if a message has been received
+			if (resp.id) {
+				console.log("received message:", resp.message);
+
+				// we are done with working on our message, we can now safely delete it
+				rsmq.deleteMessage({ qname: queuename, id: resp.id }, (err) => {
+					if (err) {
+						console.error(err);
+						return;
+					}
+
+					console.log("deleted message with id", resp.id);
+				});
+			} else {
+				console.log("no available message in queue..");
+			}
+		});
+	}, 500);
+}
